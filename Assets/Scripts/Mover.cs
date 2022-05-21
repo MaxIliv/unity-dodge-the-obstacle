33f22bf53f332371bc7ca6bac5e000c9656d2d2f
MaxIliv <max.r.iliv@gmail.com>
2022-05-22 00:47:23 +0300

    [DTO] Serialize Fields

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float xValue = 0f;
+    [SerializeField] float yValue = 0.01f;
+    [SerializeField] float zValue = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // transform.Translate(1, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(xValue, yValue, zValue);
     }
 }
